228e253418a3eb4961a57c3e87c65e60bf954b83
satam6203 <satyamkumarsinghjaisidih@gmail.com>
2025-11-29 12:53:08 +0530

    files added

---
+++ catify/src/customer/pages/Auth/Auth.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import LoginForm from './LoginForm'
+import { Alert, Button, Snackbar } from '@mui/material';
+import SignupForm from './SignupForm';
+import { useAppSelector } from '../../../Redux Toolkit/Store';
+
+const Auth = () => {
+    const [isLoginPage, setIsLoginPage] = useState(true);
+    const handleCloseSnackbar = () => setSnackbarOpen(false)
+    const { auth } = useAppSelector(store => store)
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+    useEffect(() => {
+
+        if (auth.otpSent || auth.error) {
+            setSnackbarOpen(true);
+            console.log("store ", auth.error)
+        }
+
+    }, [auth.otpSent,auth.error])
+
+    return (
+        <div className='flex justify-center h-[90vh] items-center'>
+            <div className='max-w-md h-[85vh] rounded-md border shadow-lg '>
+                <img className='w-full rounded-t-md' 
+                src="https://static.vecteezy.com/system/resources/thumbnails/004/299/835/small/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" 
+                alt="" />
+                <div className='mt-8 px-10'>
+                    {isLoginPage ? <LoginForm /> : <SignupForm />}
+
+                    <div className='flex items-center gap-1 justify-center mt-5'>
+                        <p>{isLoginPage && "Don't"} have Account ?</p>
+                        <Button onClick={() => setIsLoginPage(!isLoginPage)} size='small'>{isLoginPage ? "create account" : "login"}</Button>
+                    </div>
+                </div>
+
+
+            </div>
+            <Snackbar
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                open={snackbarOpen} autoHideDuration={6000}
+                onClose={handleCloseSnackbar}
+            >
+                <Alert
+                    onClose={handleCloseSnackbar}
+                    severity={auth.error?"error":"success"}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    {auth.error?auth.error : " otp sent to your email!"}
+                </Alert>
+            </Snackbar>
+        </div>
+    )
+}
+
+export default Auth
